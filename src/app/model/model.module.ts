112e9e9cc3fcd99679d633e528bd62f12ce819e3
Augusto Acual <augustoacual@gmail.com>
2025-08-12 12:31:14 -0600

    Create the Cart Model and expose as service

--- src/app/model/model.module.ts
+++ src/app/model/model.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core'
 import { ProductRespository } from './product.repository'
 import { StaticDataSource } from './static.datasource'
+import { Cart } from './cart.model'
 
 @NgModule({
-  providers: [ProductRespository, StaticDataSource],
+  providers: [ProductRespository, StaticDataSource, Cart],
 })
 export class ModelModule {}
