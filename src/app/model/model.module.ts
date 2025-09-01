3bf69b87df6acbee6697229f4fb1f33c1b3479c8
Augusto Acual <augustoacual@gmail.com>
2025-09-01 11:01:14 -0600

    Processing Orders

--- src/app/model/model.module.ts
+++ src/app/model/model.module.ts
@@ -2,8 +2,16 @@ import { NgModule } from '@angular/core'
 import { ProductRespository } from './product.repository'
 import { StaticDataSource } from './static.datasource'
 import { Cart } from './cart.model'
+import { Order } from './order.model'
+import { OrderRepository } from './order.repository'
 
 @NgModule({
-  providers: [ProductRespository, StaticDataSource, Cart],
+  providers: [
+    ProductRespository,
+    StaticDataSource,
+    Cart,
+    Order,
+    OrderRepository,
+  ],
 })
 export class ModelModule {}
